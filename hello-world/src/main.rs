b3fba7f03bc6d2f776055b40d9a92c4176e6e5ad
vikyw <vikyw89@gmail.com>
2025-01-12 12:15:50 +0800

    feat: init

---
+++ hello-world/src/main.rs
@@ -0,0 +1,26 @@
+use std::io::{prelude::*, BufReader};
+use std::net::{TcpListener, TcpStream};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(stream: TcpStream) {
+    println!("Connection established!");
+
+    let buf_reader = BufReader::new(stream);
+    println!("Request: {buf_reader:#?}");
+    let http_request = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect::<Vec<String>>();
+
+    println!("Request: {http_request:#?}");
+}
