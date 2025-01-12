b6cff9d6c4c303a076d0379d49ad4d03c72c90c5
vikyw <vikyw89@gmail.com>
2025-01-12 14:00:09 +0800

    fix: pure function passed by refference

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,26 +1,36 @@
-use std::io::{prelude::*, BufReader};
-use std::net::{TcpListener, TcpStream};
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        let unwrapped_stream = stream.unwrap();
 
-        handle_connection(stream);
+        handle_connection(&unwrapped_stream);
     }
 }
 
-fn handle_connection(stream: TcpStream) {
-    println!("Connection established!");
-
+fn handle_connection(stream: &TcpStream) {
     let buf_reader = BufReader::new(stream);
-    println!("Request: {buf_reader:#?}");
-    let http_request = buf_reader
+    let http_request: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
-        .collect::<Vec<String>>();
+        .collect();
+    println!("{:?}", http_request);
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("./hello-world/hello.html").unwrap();
+    let length = contents.len();
 
-    println!("Request: {http_request:#?}");
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream
+        .try_clone()
+        .unwrap()
+        .write_all(response.as_bytes())
+        .unwrap();
 }
